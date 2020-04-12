727604e7d15695eef95160c40c394898b61eef6b
Alexa <alexakearns@outlook.com>
2020-04-12 20:13:01 +0100

    return docked bike - green

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,16 @@
 class DockingStation
 
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
   def dock(bike)
-    bike
+    @bike = bike
   end
 
   def bike
-    
+    @bike
   end
 end
