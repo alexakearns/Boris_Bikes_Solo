79eb683af125ae73534a9f6119a4399fc99f1118
Alexa <alexakearns@outlook.com>
2020-04-13 08:41:39 +0100

    release bike method errors if no bikes docked - green

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -12,6 +12,10 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+
+    it 'raises an error when no bike available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
   end
 
   it 'releases working bikes' do
